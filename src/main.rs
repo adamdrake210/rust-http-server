1d3e6d165190ea773fcedd9db9399cc26a1f15ba
Adam Drake <adam@chemify.io>
2022-10-27 17:29:07 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(); 
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
